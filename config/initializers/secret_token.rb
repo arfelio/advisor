7765d941bf167a19acbcdf232d600b3b09ebebc9
arfelio <arfelio1980@gmail.com>
2014-07-02 19:43:13 +0300

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Advisor::Application.config.secret_token = 'cf7eafaaaab289a896cd35eb48a05f12b830dc56b2092c9b1bd3d0a0755df50a748127f9ca6853aa7768c13c6f00f137d46a44192f912318911885e8feee0048'
